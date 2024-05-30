1603780cbc906ef206a03f39b2fd84445c85f820
Deraoui Abdelmaoula <abdelmaoula.deraoui@bell.ca>
2024-05-30 09:44:27 +0100

    add implementation of RoomDatabase

--- app/src/main/java/ma/kader/cosmeticapp/data/data_source/ProductDatabase.kt
+++ app/src/main/java/ma/kader/cosmeticapp/data/data_source/ProductDatabase.kt
@@ -1,13 +1,14 @@
 package ma.kader.cosmeticapp.data.data_source
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import ma.kader.cosmeticapp.domain.model.Product
 
 @Database(
     entities = [Product::class],
     version = 1
 )
-abstract class ProductDatabase {
+abstract class ProductDatabase: RoomDatabase() {
 
     abstract val dao: ProductDao
     companion object {
