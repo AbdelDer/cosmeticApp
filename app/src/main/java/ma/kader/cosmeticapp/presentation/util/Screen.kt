273cc89382ee7f9b8c1172fd327b5438e88a28ac
Deraoui Abdelmaoula <abdelmaoula.deraoui@bell.ca>
2024-05-30 11:25:26 +0100

    convert to data object since we upgraded the kotlin version and now we have data object this will make the code more consistent

--- app/src/main/java/ma/kader/cosmeticapp/presentation/util/Screen.kt
+++ app/src/main/java/ma/kader/cosmeticapp/presentation/util/Screen.kt
@@ -1,6 +1,6 @@
 package ma.kader.cosmeticapp.presentation.util
 
 sealed class Screen(val route: String) {
-    object HomeScreen: Screen("home_screen")
-    object AddEditProductScreen: Screen("add_edit_product_screen")
+    data object HomeScreen: Screen("home_screen")
+    data object AddEditProductScreen: Screen("add_edit_product_screen")
 }
